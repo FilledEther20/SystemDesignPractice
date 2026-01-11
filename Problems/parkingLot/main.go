e472a499c293b60c1a239754056d1ca1568ce079
Chaitanya Gairola <gairolachaitanya@gmail.com>
2026-01-12 00:00:03 +0530

    Strategy pattern

--- Problems/parkingLot/main.go
+++ Problems/parkingLot/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 type VehicleSize int
 type SpotType int
@@ -66,6 +69,55 @@ type Ticket struct {
 	EntryTime    time.Time
 }
 
+/*
+	Strategy Pattern for fees calculation
+*/
 type FareStrategy interface {
-	Calculate(t Ticket, vsize VehicleSize, spotType SpotType, duration time.Time)
+	Calculate(t Ticket, vsize VehicleSize, duration float64) float64
 }
+
+type BaseFareStrategy struct{}
+
+func (b *BaseFareStrategy) Calculate(t Ticket, vSize VehicleSize, durationHours float64) float64 {
+	rate := 0.0
+	switch vSize {
+	case Small:
+		rate = 10.0
+	case Medium:
+		rate = 20.0
+	case Large:
+		rate = 30.0
+	}
+	return rate * durationHours
+}
+
+type PeakHoursFareStrategy struct{}
+
+func (p *PeakHoursFareStrategy) Calculate(t Ticket, vSize VehicleSize, durationHours float64) float64 {
+	baseStrategy := &BaseFareStrategy{}
+	baseFee := baseStrategy.Calculate(t, vSize, durationHours)
+
+	currentHour := time.Now().Hour()
+	if currentHour >= 17 && currentHour <= 20 {
+		return baseFee * 1.5
+	}
+	return baseFee
+}
+
+type FareCalculator struct {
+	strategy FareStrategy
+}
+
+func (fc *FareCalculator) CalculateFee(t Ticket, vSize VehicleSize) float64 {
+	duration := time.Since(t.EntryTime)
+	hours := math.Ceil(duration.Hours())
+	if hours == 0 {
+		hours = 1
+	}
+	return fc.strategy.Calculate(t, vSize, hours)
+}
+
+
+/*
+	
+*/
