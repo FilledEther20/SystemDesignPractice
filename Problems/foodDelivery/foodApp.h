52f073f93618d616517624313091ff4da9844b7b
Chaitanya Gairola <gairolachaitanya@gmail.com>
2026-02-14 23:52:14 +0530

    FoodDelivery example

---
+++ Problems/foodDelivery/foodApp.h
@@ -0,0 +1,18 @@
+#ifndef FOOD_APP_H
+#define FOOD_APP_H
+
+#include<vector>
+#include <string>
+
+using namespace std;
+
+class FoodApp{
+public:
+    FoodApp(){
+        initializeRestaurant();
+    }
+
+    void initializeRestaurant(){
+
+    }
+}
