bc28df1fcf069b8f3aa68905f3effec5b1aa9018
Chaitanya Gairola <gairolachaitanya@gmail.com>
2026-04-25 23:04:41 +0530

    factory method

---
+++ Patterns/factoryMethod.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+using namespace std;
+
+class Burger {
+public:
+  virtual void prepare() = 0;
+};
+
+class Factory {
+public:
+  virtual Burger *createBurger(string &type) = 0;
+};
+class StandardBurger : public Burger {
+public:
+  void prepare() override {
+    cout << "Preparing Standard Burger with bun, patty, cheese, and lettuce!"
+         << endl;
+  }
+};
+
+class PremiumBurger : public Burger {
+public:
+  void prepare() override {
+    cout << "Preparing Premium Burger with gourmet bun, premium patty, cheese, "
+            "lettuce, and secret sauce!"
+         << endl;
+  }
+};
+
+class BasicWheatBurger : public Burger {
+public:
+  void prepare() override {
+    cout << "Preparing Basic Wheat Burger with bun, patty, and ketchup!"
+         << endl;
+  }
+};
+
+class StandardWheatBurger : public Burger {
+public:
+  void prepare() override {
+    cout << "Preparing Standard Wheat Burger with bun, patty, cheese, and "
+            "lettuce!"
+         << endl;
+  }
+};
+
+class PremiumWheatBurger : public Burger {
+public:
+  void prepare() override {
+    cout << "Preparing Premium Wheat Burger with gourmet bun, premium patty, "
+            "cheese, lettuce, and secret sauce!"
+         << endl;
+  }
+};
+
+class BasicBurger : public Burger {
+public:
+  void prepare() override {
+    cout << "Preparing Basic Burger with bun, patty, and ketchup!" << endl;
+  }
+};
+
+class SinghBurger : public Factory {
+public:
+  Burger *createBurger(string &type) override {
+    if (type == "basic") {
+      return new BasicBurger();
+    } else if (type == "standard") {
+      return new StandardBurger();
+    } else if (type == "premium") {
+      return new PremiumBurger();
+    } else {
+      cout << "Invalid burger type! " << endl;
+      return nullptr;
+    }
+  }
+};
+
+class KindFactory : public Factory {
+public:
+  Burger *createBurger(string &type) override {
+    if (type == "basic") {
+      return new BasicWheatBurger();
+    } else if (type == "standard") {
+      return new StandardWheatBurger();
+    } else if (type == "premium") {
+      return new PremiumWheatBurger();
+    } else {
+      cout << "Invalid burger type! " << endl;
+      return nullptr;
+    }
+  }
+};
